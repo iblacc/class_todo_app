9fcf7baf6f378ddf3a24372a8e621039b022d454
Shuaib Bashir <bashonbash@gmail.com>
2022-06-16 13:28:38 +0100

    second class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
+let id = 0;
 function App() {
-  let [title, setTitle] = useState("");
-  let [description, setDescription] = useState("");
-  let [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editId, setEditId] = useState(null);
 
   function handleChange(e) {
     let name = e.target.name;
@@ -18,12 +21,48 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
+  }
 
+  function addTodo() {
     setTodos(function (prevTodos) {
-      let newTodo = { title: title, desc: description };
+      let newTodo = { id: ++id, title: title, desc: description };
       let updated = [...prevTodos, newTodo];
       return updated;
     });
+
+    setTitle("");
+    setDescription("");
+  }
+
+  function updateTodo() {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === editId) {
+        return {
+          id: editId,
+          title: title,
+          desc: description,
+        };
+      }
+
+      return todo;
+    });
+    setTodos([...updatedTodos]);
+
+    setTitle("");
+    setDescription("");
+    setEditMode(false);
+  }
+
+  function handleEdit(editId) {
+    setEditId(editId);
+    setEditMode(true);
+    const todoToEdit = todos.find((todo) => {
+      return todo.id === editId;
+    });
+
+    console.log(todoToEdit);
+    setTitle(todoToEdit.title);
+    setDescription(todoToEdit.desc);
   }
 
   return (
@@ -31,17 +70,31 @@ function App() {
       <div className="form">
         <h2>Todo Form</h2>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="title">Title</label>
-          <input onChange={handleChange} type="text" name="title" id="title" />
-          <label htmlFor="description">Description</label>
-          <textarea onChange={handleChange} name="desc" id="description" />
-          <button>Add</button>
-
           <div>
-            Title: {title}
-            <br />
-            Desc: {description}
+            <label htmlFor="title">Title</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              value={title}
+              name="title"
+              id="title"
+            />
           </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea
+              onChange={handleChange}
+              value={description}
+              name="desc"
+              id="description"
+            />
+          </div>
+
+          {editMode ? (
+            <button onClick={updateTodo}>Update</button>
+          ) : (
+            <button onClick={addTodo}>Add</button>
+          )}
         </form>
       </div>
       <div className="list">
@@ -49,7 +102,13 @@ function App() {
         <ul>
           {todos.map((todo, index) => (
             <li key={index}>
-              <h5>{todo.title}</h5>
+              <div className="item-title">
+                <h5>
+                  {index + 1}. {todo.title}
+                </h5>
+                <button onClick={() => handleEdit(todo.id)}>Edit</button>
+                <button>delete</button>
+              </div>
               <p>{todo.desc}</p>
             </li>
           ))}
